73e50841871592ed141e60cf4104fd7d2e4f2850
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-05-03 17:53:32 -0300

    It's getting serious!

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var webpackStream = require('webpack-stream');
+var del = require('del');
 
-gulp.task('default', () => {
-  return gulp.src('src/app.js')
+gulp.task('babel', () => {
+  return gulp.src('src/core/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('webpack', ['babel'], () => {
+  return gulp.src('dist/*.js')
+    .pipe(webpackStream({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['webpack'], () => {
+  del(['dist/GitHubAPI.js']);
 });
---
+++ src/core/GitHubAPI.js
@@ -0,0 +1,10 @@
+// 'use strict'
+var angular = require('angular');
+
+class GitHubAPI{
+  static fake(angular){
+    console.log(angular);
+  }
+}
+
+GitHubAPI.fake(angular);
